96f60201cc8c3ab5e11f1767e0dadaf8337173bb
cprincec <chukwuprincec@gmail.com>
2022-06-07 10:08:51 +0100

    set

--- final-project/scripts/admission.js
+++ final-project/scripts/admission.js
@@ -1,1 +1,43 @@
-document.querySelector(".submit").addEventListener("click", function () {document.querySelector("form").submit()})
+const aStudent = {};
+
+function createStudent() {
+    let lastName = document.querySelector("#lname").value;
+    let firstName = document.querySelector("#fname").value;
+    let middleName = document.querySelector("#mname").value;
+    let dateOfBirth = document.querySelector("#dob").value;
+    let gender = document.querySelector("#gender").value;
+    let address = document.querySelector("#raddress").value;
+    let nationality = document.querySelector("#nationality").value;
+    let stateOfOrigin = document.querySelector("#sorigin").value;
+    let lga = document.querySelector("#lga").value;
+    let sClass = document.querySelector("#class").value;
+
+    aStudent.studentName = firstName + " " + middleName + " " + lastName
+    aStudent.studentDOB = dateOfBirth
+    aStudent.studentGender = gender
+    aStudent.studentAddress = address
+    aStudent.studentNationality = nationality
+    aStudent.studentState = stateOfOrigin
+    aStudent.studentLga = lga
+    aStudent.studentClass = sClass
+    aStudent.studentNumber = students.length
+    
+    createProfile(aStudent)
+}
+
+
+function createProfile()    {
+    let genderHtml = document.querySelector("#profileGender")
+    let DOBHtml = document.querySelector("#profileDob")
+    let addressHtml = document.querySelector("#profileAddress")
+    let nationalityHtml = document.querySelector("#profileNationality")
+    let stateHtml = document.querySelector("#profileState")
+    let lgaHtml = document.querySelector("#profileLga")
+    let classHtml = document.querySelector("#profileClass")
+
+    
+
+}
+
+document.querySelector(".submit").addEventListener("click", createStudent);
+console.log(students);
